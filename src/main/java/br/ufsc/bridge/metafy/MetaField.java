cd8873f2a6553c120e55fad17e7b06ed3327b8a3
paulojair <paulinhojair@gmail.com>
2016-08-22 10:45:21 -0300

    equals hashcode pra metafy encadeado

--- src/main/java/br/ufsc/bridge/metafy/MetaField.java
+++ src/main/java/br/ufsc/bridge/metafy/MetaField.java
@@ -29,6 +29,52 @@ public class MetaField<T> {
 		return this.type;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (this.alias == null ? 0 : this.alias.hashCode());
+		result = prime * result + (this.parent == null ? 0 : this.parent.hashCode());
+		result = prime * result + (this.type == null ? 0 : this.type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		MetaField other = (MetaField) obj;
+		if (this.alias == null) {
+			if (other.alias != null) {
+				return false;
+			}
+		} else if (!this.alias.equals(other.alias)) {
+			return false;
+		}
+		if (this.parent == null) {
+			if (other.parent != null) {
+				return false;
+			}
+		} else if (!this.parent.equals(other.parent)) {
+			return false;
+		}
+		if (this.type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		} else if (!this.type.equals(other.type)) {
+			return false;
+		}
+		return true;
+	}
+
 	public MetaField<?> getParent() {
 		return this.parent;
 	}
