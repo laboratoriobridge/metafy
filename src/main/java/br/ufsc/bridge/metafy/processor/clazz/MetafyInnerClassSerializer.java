0bb2a61d8777f65a72fc78d758a91c5e52d1344d
Felipe Taboada <fliptaboada@gmail.com>
2018-03-27 14:34:13 -0300

    fix generic class

--- src/main/java/br/ufsc/bridge/metafy/processor/clazz/MetafyInnerClassSerializer.java
+++ src/main/java/br/ufsc/bridge/metafy/processor/clazz/MetafyInnerClassSerializer.java
@@ -8,30 +8,15 @@ public class MetafyInnerClassSerializer {
 		String metaClassName = data.getSimpleName();
 		String metaReferenceName = data.getSimpleReferenceName();
 
-		pw.println(String.format("private static class %s extends MetaBean<%s> {", metaClassName, metaReferenceName));
-		pw.println();
-
-		pw.println(String.format("\tpublic static final %s meta = new %s();", metaClassName, metaClassName));
+		pw.println(String.format("public static class %s extends MetaBean<%s> {", metaClassName, metaReferenceName));
 		pw.println();
 
 		data.getAttributes().stream().forEach(attribute -> attribute.writeAttribute(pw));
 
 		pw.println();
 
-		// Default construtor
-		pw.println(String.format("\tpublic %s() {", metaClassName));
-		pw.println(String.format("\t\tsuper(%s.class);", metaReferenceName));
-		pw.println(String.format("\t}"));
-		pw.println();
-
-		// Construtor with parent
-		pw.println(String.format("\tpublic %s(MetaBean<?> parent) {", metaClassName));
-		pw.println(String.format("\t\tsuper(parent, %s.class);", metaReferenceName));
-		pw.println(String.format("\t}"));
-		pw.println();
-
 		// Construtor with parent and alias
-		pw.println(String.format("\tpublic %s(MetaBean<?> parent, String alias) {", metaClassName));
+		pw.println(String.format("\tprotected %s(MetaBean<?> parent, String alias) {", metaClassName));
 		pw.println(String.format("\t\tsuper(parent, %s.class, alias);", metaReferenceName));
 		pw.println(String.format("\t}"));
 		pw.println();
